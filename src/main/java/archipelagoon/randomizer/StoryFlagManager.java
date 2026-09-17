a1dc43a2d19b3e9487dab43efb4e2ca57ab51817
Zdera <alexander.milliken21@gmail.com>
2026-09-16 19:55:56 -0500

    Bugfix switch statement and add forest merchant code. Unable to test merchant due to hard crash atm

--- src/main/java/archipelagoon/randomizer/StoryFlagManager.java
+++ src/main/java/archipelagoon/randomizer/StoryFlagManager.java
@@ -35,19 +35,37 @@ public final class StoryFlagManager {
   public static void readScriptFlags(final ReadGlobalFlagsEvent event) {
     if(event.flagArray == ScriptFlagArrayEnum.FLAGS2) {
       switch(event.getFlagIndex()) {
+        case 11:
+          //Forest Merchant
+          if (current_submap == 624){
+            event.flagValue = false;
+          }
+        case 16:
+          //Forest Merchant
+          if (current_submap == 624){
+            event.flagValue = false;
+          }
         case 59:
+          //Nest of Dragon plant
           event.flagValue = gameState_800babc8.goods_19c.has(GameEngine.REGISTRIES.goods.getEntry(LodGoods.LIFE_WATER.getId()));
+          break;
         case 58:
+          //Nest of Dragon life water puddle
           event.flagValue = gameState_800babc8.goods_19c.has(GameEngine.REGISTRIES.goods.getEntry(LodGoods.WATER_BOTTLE.getId()));
+          break;
         case 9:
+          //Hellena Shana's prison tower
           event.flagValue = gameState_800babc8.goods_19c.has(GameEngine.REGISTRIES.goods.getEntry(LodGoods.PRISON_KEY.getId()));
+          break;
         case 203:
           event.flagValue = gameState_800babc8.goods_19c.has(GameEngine.REGISTRIES.goods.getEntry(LodGoods.BOAT_LICENSE.getId()));
+          break;
       }
     } else if(event.flagArray == ScriptFlagArrayEnum.FLAGS1) {
       switch(event.getFlagIndex()) {
         case 1:
           event.flagValue = gameState_800babc8.goods_19c.has(GameEngine.REGISTRIES.goods.getEntry(LodGoods.AXE_FROM_THE_SHACK.getId()));
+          break;
       }
     }
   }
